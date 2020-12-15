9a78da4a7f6a904380dfaf66ca8161ba75bff4a5
88jina <jina940323@gmail.com>
2020-12-16 00:01:04 +0900

    controller function

--- src/main/java/com/example/assignment/controller/StudentController.java
+++ src/main/java/com/example/assignment/controller/StudentController.java
@@ -2,20 +2,41 @@ package com.example.assignment.controller;
 
 import com.example.assignment.entity.Student;
 import com.example.assignment.repository.StudentRepository;
+import com.example.assignment.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class StudentController {
 
     @Autowired
-    private StudentRepository repository;
+    private StudentService service;
 
     @PostMapping("/add")
-    public Student add(Student student){
-        Student newStudent = repository.save(student);
-        return newStudent;
+    public Student addStudent(@RequestBody Student student) {
+        return service.saveStudent(student);
+    }
+
+    @GetMapping("/students")
+    public List<Student> getAllStudents() {
+        return service.getStudents();
+    }
+
+    @GetMapping("/student/{name}")
+    public Student getStudentByName(@PathVariable String name) {
+        return service.getStudentByName(name);
+    }
+
+    @PutMapping("/update")
+    public Student updateStudent(@RequestBody Student student) {
+        return service.updateStudent(student);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteStudent(@PathVariable long id) {
+        return service.deleteStudent(id);
     }
 
 }
